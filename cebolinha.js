0683b826c54d9864e70a306fb54bedc837f1d4d2
Diego Niemeyer <102764313+dniemeryers@users.noreply.github.com>
2022-08-08 00:32:23 -0300

    inittial

---
+++ cebolinha.js
@@ -0,0 +1,31 @@
+const textarea = document.querySelector("textarea");
+const button2 = document.querySelector("#button2");
+const button1 = document.querySelector("#button1");
+button2.addEventListener('click', () =>  falaCebolinha());
+button1.addEventListener('click', () => limpar());
+
+function falaCebolinha(){
+    const frase = document.querySelector("#frase").value;
+    
+    let fraseCebolinha="";
+    
+    for (let letra of frase) {
+        if (letra==="r"){
+            fraseCebolinha+="l";
+    
+        }else{
+            fraseCebolinha += letra ; 
+        }
+     }
+    return textarea.value = fraseCebolinha;
+    }
+  function limpar(){
+ textarea.value="";
+ frase.value="";
+  }
+ 
+    
+    
+        
+        
+      
